70e137354da36422e6b62fe33e9d0b463438e82e
Appleby43 <alexappleby70@gmail.com>
2020-09-25 18:48:28 -0600

    wrote Environment instantiation

--- src/main/java/com/alexblakeappleby/cepsim/model/env/Tile.java
+++ src/main/java/com/alexblakeappleby/cepsim/model/env/Tile.java
@@ -2,6 +2,10 @@ package com.alexblakeappleby.cepsim.model.env;
 
 import com.alexblakeappleby.cepsim.model.species.Organism;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * A tile is one 'microhabitat' that makes up a larger environment.
  * Each tile can only contain one organism.
@@ -9,7 +13,9 @@ import com.alexblakeappleby.cepsim.model.species.Organism;
 public class Tile {
     private State state = State.FREE;
     private Organism inhabitant = null;
-    private Tile[] neighbors;
+    private Set<Tile> neighbors = new HashSet<>();
+
+    private int x, y;
 
     public enum State {
         FREE,
@@ -17,8 +23,21 @@ public class Tile {
         REGENERATING,
     }
 
-    public Tile(Tile[] neighbors){
-        this.neighbors = neighbors;
+    public Tile(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void addNeighbor(Tile neighbor){
+        if(neighbors.size() >= 6){
+            throw new Error("this tile has too many neighbors. Check to make sure the environment was created correctly.");
+        }
+
+        neighbors.add(neighbor);
+    }
+
+    public Set<Tile> getNeighbors (){
+        return neighbors;
     }
 
     public boolean isInhabited(){
---
+++ src/test/java/com/alexblakeappleby/cepsim/model/env/EnvironmentTest.java
@@ -0,0 +1,49 @@
+package com.alexblakeappleby.cepsim.model.env;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class EnvironmentTest {
+
+    @Test
+    public void test1CellEnv() {
+        Environment e = new Environment(1);
+
+        assertEquals(1, e.cellCount);
+
+        Tile t = e.getTile(0,0);
+
+        assertEquals(0, t.getNeighbors().size());
+    }
+
+    @Test
+    public void test9CellEnv() {
+        Environment e = new Environment(3);
+
+        assertEquals(9, e.cellCount);
+
+        Tile middleTile = e.getTile(1,1);
+
+        assertEquals(6, middleTile.getNeighbors().size());
+
+        Set<Tile> targetSet = new HashSet<>();
+        targetSet.add(e.getTile(0,0));
+        targetSet.add(e.getTile(1,0));
+        targetSet.add(e.getTile(2,1));
+        targetSet.add(e.getTile(2,2));
+        targetSet.add(e.getTile(1,2));
+        targetSet.add(e.getTile(0,1));
+
+        assertEquals(targetSet, middleTile.getNeighbors());
+
+        Tile cornerTile1 = e.getTile(2,2);
+        assertEquals(3, cornerTile1.getNeighbors().size());
+
+        Tile cornerTile2 = e.getTile(2,0);
+        assertEquals(2, cornerTile2.getNeighbors().size());
+    }
+}
